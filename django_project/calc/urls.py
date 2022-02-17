652bbf7368e6ea556c041a84bd8175be527f4435
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-02-16 21:13:41 -0700

    added comment

--- django_project/calc/urls.py
+++ django_project/calc/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import HomePageView, CalcPageView
 from calc import views
 
+# this tells what url to go to and calls functions before going to that url
 urlpatterns = [
     path("calc/", CalcPageView.as_view(), name="calc"),
     path("calc/answer/", views.answer, name="answer"),
